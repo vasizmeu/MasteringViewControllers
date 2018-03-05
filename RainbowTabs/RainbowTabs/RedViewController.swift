9635adb7327db2da469e6fee638de875f8885f9e
Vasilica Costescu <vasilica.costescu@gmail.com>
2018-03-05 15:45:38 +0000

    Add RainbowTabs project.

---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Vasilica Costescu on 04/03/2018.
+//  Copyright © 2018 Vasilica Costescu. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
